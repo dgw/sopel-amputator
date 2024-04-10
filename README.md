b5bbe58abe950781f0ddcc33f7bdffbe68cff796
dgw <dgw@technobabbl.es>
2024-04-10 12:09:17 -0500

    Minimum viable plugin
    - Detects suspected AMP links
    - Calls AmputatorBot API to fetch canonical form of suspected AMP links
    - Silently ignores any false-positives for which the API returns errors
    
    Future enhancements could include more granular error handling (API down
    vs. "no AMP link detected"), configurable list of domains to ignore
    (e.g. `bandcamp.com` contains an AMP substring but isn't AMP), user
    opt-out and/or support for global "link exclusion character" when
    implemented (see upstream issue, sopel-irc/sopel#2283).

---
+++ README.md
@@ -0,0 +1,17 @@
+# sopel-amputator
+
+Sopel plugin that detects AMP links and finds their canonical forms using AmputatorBot
+
+## Installing
+
+Releases are hosted on PyPI, so after installing Sopel, all you need is `pip`:
+
+```shell
+$ pip install sopel-amputator
+```
+
+## Configuring
+
+The easiest way to configure `sopel-amputator` is via Sopel's
+configuration wizard—simply run `sopel-plugins configure amputator`
+and enter the values for which it prompts you.
---
+++ sopel_amputator/__init__.py
@@ -0,0 +1,53 @@
+"""sopel-amputator
+
+Sopel plugin that detects AMP links and finds their canonical forms using AmputatorBot
+"""
+from __future__ import generator_stop
+
+import re
+
+import requests
+
+from sopel import plugin, tools
+
+
+LOGGER = tools.get_logger('amputator')
+AMPUTATOR_CONVERT_API = 'https://www.amputatorbot.com/api/v1/convert'
+# Substrings borrowed from AmputatorBot:
+# https://github.com/KilledMufasa/AmputatorBot/blob/263ff4b3b9c5220f6d0dc3479d8640dca7aafe15/static/static.txt#L8-L9
+AMP_KEYWORDS = ["/amp", "amp/", ".amp", "amp.", "?amp", "amp?", "=amp",
+                "amp=", "&amp", "amp&", "%%amp", "amp%", "_amp", "amp_"]
+
+
+def amp_patterns(settings):
+    amp_group = '|'.join(map(re.escape, AMP_KEYWORDS))
+    amp_link_pattern = 'https?://.*({amp_group}).*'.format(amp_group=amp_group)
+    return [re.compile(amp_link_pattern)]
+
+
+@plugin.url_lazy(amp_patterns)
+@plugin.output_prefix('[AMPutator] ')
+def amputate(bot, trigger):
+    suspected_amp_link = trigger.group(0)
+    params = {
+        'gac': 'true',
+        'md': 3,
+        'q': suspected_amp_link,
+    }
+
+    # Using `params=` with a dict directly means requests will urlencode the query
+    # and AmputatorBot API doesn't handle that (April 2024)
+    r = requests.get(
+        AMPUTATOR_CONVERT_API,
+        params='&'.join("%s=%s" % (k,v) for k,v in params.items()))
+
+    try:
+        r.raise_for_status()
+    except Exception:
+        LOGGER.info(
+            'AmputatorBot API returned an error; ignoring suspected AMP link %r' % suspected_amp_link)
+        return plugin.NOLIMIT
+
+    data = r.json()[0]
+    if data['canonical']:
+        bot.say("{}'s canonical link: {}".format(trigger.nick, data['canonical']['url']))
